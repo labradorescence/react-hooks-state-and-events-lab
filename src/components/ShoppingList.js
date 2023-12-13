3c50ba7896a726f40561e73a3e336788de24d6b2
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-12-13 12:38:04 -0500

    review

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  console.log(items)
+  const [ selectedCategory, setSelectedCategory ] = useState(items) //[]ARR
+
+  const handleCategoryChange = (event) => {
+    //console.log(event.target.value) //string
+    
+    // [] .filter
+
+    const filteredItemArr = items.filter(item => {
+      console.log(item.category, event.target.value)
+      return item.category === event.target.value
+    })
+    setSelectedCategory(filteredItemArr)
+    
+    //setSelectedCategory(event.target.value)
+    //console.log(selectedCategory)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" 
+                onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +33,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {selectedCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
